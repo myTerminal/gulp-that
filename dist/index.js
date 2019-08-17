358b1ec9691b516b0d16ae7498f59b61fe79aa1b
myTerminal <team.terminal@gmail.com>
2019-08-16 22:28:48 -0500

    sync configuration with template-lib-gulp

--- dist/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";var through=require("through2");module.exports=function(t){if(!t)throw new Error("gulp-that: Missing operation");return through.obj(function(r,o,n){r.contents=new Buffer(t(r.contents.toString()).toString()),this.push(r),n()})};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 /* global require module Buffer */
 
-var through = require('through2');
+const through = require('through2');
 
-module.exports = function (operation) {
+module.exports = operation => {
     if (!operation) {
         throw new Error('gulp-that: Missing operation');
     }
 
-    return through.obj(function(file, encoding, callback) {
-        file.contents = new Buffer(operation(file.contents.toString()).toString());
-        this.push(file);
-        callback();
-    });
+    return through.obj(
+        (file, encoding, callback) => {
+            file.contents = new Buffer(
+                operation(file.contents.toString()).toString()
+            );
+
+            this.push(file);
+
+            callback();
+        }
+    );
 };
